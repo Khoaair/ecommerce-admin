69a4c944d723feec339a6eea3f01b744c9c95b11
KhoaAir win <KhoaAir@gmail.com>
2023-10-08 22:23:20 +0700

    build dashboard

---
+++ app/(auth)/layout.jsx
@@ -0,0 +1,6 @@
+const layout = ({ children }) => {
+  return (
+    <div className='flex items-center justify-center h-full'>{children}</div>
+  );
+};
+export default layout;
---
+++ app/(root)/page.jsx
@@ -0,0 +1,10 @@
+import { UserButton } from '@clerk/nextjs';
+
+const SetupPage = () => {
+  return (
+    <div className='p-4'>
+      <UserButton></UserButton>
+    </div>
+  );
+};
+export default SetupPage;
